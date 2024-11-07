c8ffd6349f7d7bfc822ca26b41c873ee0ea49628
AlexanderLukash <sashass20062727@gmail.com>
2024-11-07 10:05:13 +0200

    Make some changes in category admin.

--- core/apps/shop/admin.py
+++ core/apps/shop/admin.py
@@ -9,3 +9,6 @@ from core.apps.shop.models.categories import Category
 @admin.register(Category)
 class CategoryAdmin(ModelAdmin):
     list_display = ("id", "name", "parent", "slug", "created_at", "updated_at")
+    list_display_links = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ["id"]
